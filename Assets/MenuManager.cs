ca36c0c1325203afe04f50ab094b936673de39a1
Sebastian K Øie <sebagnus@gmail.com>
2022-11-07 17:33:25 +0100

    Menu Manager
    A script to give menu function.
    Also deals with Playerprefs to store Highscore, high score holder name & Current player name.

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField nameInput;
+    public TextMeshProUGUI highScoreDisplay;
+
+    private void Start()
     {
-        
+        string score = PlayerPrefs.GetInt("HighScore").ToString();
+        string bName = PlayerPrefs.GetString("BestPlayer");
+
+        highScoreDisplay.text = "High Score: " + bName + " " + score;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PlayerPrefs.SetString("CurrentPlayer", nameInput.text);
+    }
+
+
+    public void StartGame()
+    {
+        //Load Game Scene (Consistent as 1, only 2 scenes to ever exist)
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
